a2d04ac10cb39363b183fc0b9cc2631561109ab8
Raghav Gupta <55792366+RaghavGupta70@users.noreply.github.com>
2020-10-01 10:50:19 +0530

    Add files via upload

---
+++ PersonalData.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment1
+{
+    /// <summary>
+    /// A class holding personal data for a person
+    /// </summary>
+    public class PersonalData
+    {
+        #region Fields
+
+        // declare your fields here
+        string firstName="";
+        string middleName="";
+        string lastName="";
+        string city="";
+        string streetAddress="";
+        string state="";
+        string postalCode="";
+        string country="";
+        string phoneNumber="";
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the person's first name
+        /// </summary>
+        public string FirstName
+        {
+            get 
+            {
+                // delete code below and replace with correct code
+                return firstName;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's middle name
+        /// </summary>
+        public string MiddleName
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return middleName;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's last name
+        /// </summary>
+        public string LastName
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return lastName;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's street address
+        /// </summary>
+        public string StreetAddress
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return streetAddress;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's city or town
+        /// </summary>
+        public string City
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return city;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's state or province
+        /// </summary>
+        public string State
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return state;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's postal code
+        /// </summary>
+        public string PostalCode
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return postalCode;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's country
+        /// </summary>
+        public string Country
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return country;
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's phone number (digits only, no 
+        /// parentheses, spaces, or dashes)
+        /// </summary>
+        public string PhoneNumber
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return phoneNumber;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// Reads personal data from a file. If the file
+        /// read fails, the object contains an empty string for all
+        /// the personal data
+        /// </summary>
+        /// <param name="fileName">name of file holding personal data</param>
+        public PersonalData(string fileName)
+        {
+            // your code can assume we know the order in which the
+            // values appear in the string; it's the same order as
+            // they're listed for the properties above. We could do 
+            // something more complicated with the names and values, 
+            // but that's not necessary here
+
+            // IMPORTANT: The mono compiler the automated grader uses
+            // does NOT support the string Split method. You have to 
+            // use the IndexOf method to find comma locations and the
+            // Substring method to chop off the front of the string
+            // after you extract each value to extract and save the
+            // personal data
+
+            StreamReader input=null;
+            try
+            {
+             
+                input = File.OpenText(fileName);
+                int i = 0;
+                string sample="";
+                sample = input.ReadLine();
+                
+
+                i = sample.IndexOf(',');
+                firstName = sample.Substring(0, i);
+
+                i++;
+                sample = sample.Substring(i);
+                i = sample.IndexOf(',');
+                middleName = sample.Substring(0, i);
+
+
+                i++;
+                sample = sample.Substring(i);
+                i = sample.IndexOf(',');
+                lastName = sample.Substring(0, i);
+
+                i++;
+                sample = sample.Substring(i);
+                i = sample.IndexOf(',');
+                streetAddress = sample.Substring(0, i);
+
+                i++;
+                sample = sample.Substring(i);
+                i = sample.IndexOf(',');
+                city = sample.Substring(0, i);
+
+                i++;
+                sample = sample.Substring(i);
+                i = sample.IndexOf(',');
+                state = sample.Substring(0, i);
+
+                i++;
+                sample = sample.Substring(i);
+                i = sample.IndexOf(',');
+                postalCode = sample.Substring(0, i);
+
+                i++;
+                sample = sample.Substring(i);
+                i = sample.IndexOf(',');
+                country= sample.Substring(0, i);
+
+                i++;
+                sample = sample.Substring(i);
+                phoneNumber = sample;
+
+
+
+            }
+
+            catch(Exception e)
+            {
+                
+            }
+
+            finally
+            {
+                if(input==null)
+                {
+                    firstName = "";
+                    middleName = "";
+                    lastName = "";
+                    streetAddress = "";
+                    city = "";
+                    state = "";
+                    postalCode = "";
+                    country = "";
+                    phoneNumber = "";
+
+                }
+
+                else
+                {
+                    input.Close();
+                }
+                
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleCards;
+
+namespace ProgrammingAssignment2
+{
+    // IMPORTANT: Only add code in the section
+    // indicated below. The code I've provided
+    // makes your solution work with the 
+    // automated grader on Coursera
+
+    /// <summary>
+    /// Programming Assignment 2
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Implements Nothing Like Blackjack functionality
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // loop while there's more input
+            string input = Console.ReadLine();
+            while (input[0] != 'q')
+            {
+
+                // Add your code between this comment
+                // and the comment below. You can of
+                // course add more space between the
+                // comments as needed
+
+                // declare a deck variables and create a deck object
+                // DON'T SHUFFLE THE DECK
+                Deck deck = new Deck();
+
+                // deal 2 cards each to 4 players (deal properly, dealing
+                // the first card to each player before dealing the
+                // second card to each player)
+                Card card00 =  deck.TakeTopCard();
+                Card card10 = deck.TakeTopCard();
+                Card card20 = deck.TakeTopCard();
+                Card card30 = deck.TakeTopCard();
+
+                Card card01 = deck.TakeTopCard();
+                Card card11 = deck.TakeTopCard();
+                Card card21 = deck.TakeTopCard();
+                Card card31 = deck.TakeTopCard();
+
+
+
+                // deal 1 more card to players 2 and 3
+
+                Card card12 = deck.TakeTopCard();
+
+                Card card22 = deck.TakeTopCard();
+
+                // flip all the cards over
+                card00.FlipOver();
+                card01.FlipOver();
+
+                card10.FlipOver();
+                card11.FlipOver();
+                card12.FlipOver();
+
+                card20.FlipOver();
+                card21.FlipOver();
+                card22.FlipOver();
+
+                card30.FlipOver();
+                card31.FlipOver();
+
+
+
+
+                // print the cards for player 1
+              
+                Console.WriteLine(card00.Rank + "," + card00.Suit);
+                Console.WriteLine(card01.Rank + "," + card01.Suit);
+           
+                // print the cards for player 2
+              
+                Console.WriteLine(card10.Rank + "," + card10.Suit);
+                Console.WriteLine(card11.Rank + "," + card11.Suit);
+                Console.WriteLine(card12.Rank + "," + card12.Suit);
+                
+
+                // print the cards for player 3
+               
+                Console.WriteLine(card20.Rank + "," + card20.Suit);
+                Console.WriteLine(card21.Rank + "," + card21.Suit);
+                Console.WriteLine(card22.Rank + "," + card22.Suit);
+                
+
+                // print the cards for player 4
+               
+
+                Console.WriteLine(card30.Rank + "," + card30.Suit);
+                Console.WriteLine(card31.Rank + "," + card31.Suit);
+             
+                // Don't add or modify any code below
+                // this comment
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
